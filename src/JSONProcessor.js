59cbed340ad707ca9a7a6d6a9f5f2b4afebdd3cc
shunsuke maeda <duck8823@gmail.com>
2018-08-06 07:36:07 +0900

    Implement function convert to textlint AST

--- src/JSONProcessor.js
+++ src/JSONProcessor.js
@@ -1,6 +1,7 @@
 "use strict";
 
 import parse from 'json-to-ast'
+import { ASTNodeTypes } from "@textlint/ast-node-types";
 
 export class JSONProcessor {
     constructor(config = {}) {
@@ -15,11 +16,15 @@ export class JSONProcessor {
         return {
             preProcess(text) {
                 let ast = parse(text);
+                ast.type = ASTNodeTypes.Document;
+                ast.range = [ast.loc.start.column, ast.loc.end.column];
                 ast.children = ast.children.map((child) => {
+                    child = child.value;
+                    child.type = ASTNodeTypes.Str;
                     child.range = [child.loc.start.column, child.loc.end.column];
                     return child;
                 });
-                // TODO parse for textlint
+                // TODO 再帰的にする
                 return ast;
             },
             postProcess(messages, filePath) {
